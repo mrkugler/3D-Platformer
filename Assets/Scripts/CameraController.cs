bd3fff764680e8279bfd90270e13c4d402a1e1f0
Michael Kugler <mrkugler@pitt.edu>
2020-01-03 21:28:04 -0500

    First Push

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public bool useOffsetValues;
+
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+
+        //CHECK comments on these two lines, should be switched (see #9)
+        pivot.transform.parent = target.transform;
+        //pivot.transform.parent = null;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        pivot.transform.position = target.transform.position;
+        
+        //Get the x position of the mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        //Get the y position of the mouse and rotate the target
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        //pivot.Rotate(vertical, 0, 0);
+        if(invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //Limit up/down camera rotation
+        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        if(pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        //Move the camera based on the current rotation of the target and the original offset
+        float desiredYAngle = pivot.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+
+        //transform.position = target.position - offset;
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
+        }
+
+        transform.LookAt(target);
+
+    }
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+
+    public int maxHealth;
+    public int currentHealth;
+
+    public PlayerController thePlayer;
+
+    public float invincibilityLength;
+    private float invincibilityCounter;
+
+    public Renderer playerRenderer;
+    private float flashCounter;
+    public float flashLength = 0.1f;
+
+    private bool isRespawning;
+    private Vector3 respawnPoint;
+    public float respawnLength;
+
+    public GameObject deathEffect;
+    public Image blackScreen;
+    private bool isFadeToBlack;
+    private bool isFadeFromBlack;
+    public float fadeSpeed;
+    public float waitForFade;
+
+    public Text healthText;
+
+    public AudioSource backgroundMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        healthText.text = "x " + currentHealth;
+
+        //thePlayer = FindObjectOfType<PlayerController>();
+
+        respawnPoint = thePlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(invincibilityCounter > 0)
+        {
+            invincibilityCounter -= Time.deltaTime;
+
+            flashCounter -= Time.deltaTime;
+            if(flashCounter <= 0)
+            {
+                playerRenderer.enabled = !playerRenderer.enabled;
+                flashCounter = flashLength;
+            }
+
+            if(invincibilityCounter <=0 )
+            {
+                playerRenderer.enabled = true;
+            }
+        }
+
+        if(isFadeToBlack)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+            if(blackScreen.color.a == 1f)
+            {
+                isFadeToBlack = false;
+            }
+        }
+
+        if (isFadeFromBlack)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+            if (blackScreen.color.a == 0f)
+            {
+                isFadeFromBlack = false;
+            }
+        }
+    }
+
+    public void HurtPlayer(int damage, Vector3 direction)
+    {
+        if (invincibilityCounter <= 0)
+        {
+
+            currentHealth -= damage;
+            healthText.text = "x " + currentHealth;
+
+            if (currentHealth <= 0)
+            {
+                Respawn();
+                healthText.text = "x 0";
+                backgroundMusic.Stop();
+            }
+            else
+            {
+                thePlayer.Knockback(direction);
+                invincibilityCounter = invincibilityLength;
+                playerRenderer.enabled = false;
+                flashCounter = flashLength;
+            }
+        }
+    }
+
+    public void Respawn()
+    {
+        //thePlayer.transform.position - respawnPoint;
+        //currentHealth = maxHealth;
+        if(!isRespawning)
+        {
+            StartCoroutine("RespawnCo");
+        }
+    }
+
+    public IEnumerator RespawnCo()
+    {
+        isRespawning = true;
+        thePlayer.gameObject.SetActive(false);
+        Instantiate(deathEffect, thePlayer.transform.position, thePlayer.transform.rotation);
+
+        yield return new WaitForSeconds(respawnLength);
+
+        isFadeToBlack = true;
+
+        yield return new WaitForSeconds(waitForFade);
+
+        isFadeToBlack = false;
+        isFadeFromBlack = true;
+        isRespawning = false;
+
+        thePlayer.gameObject.SetActive(true);
+        thePlayer.transform.position = respawnPoint;
+        currentHealth = maxHealth;
+
+        healthText.text = "x " + maxHealth;
+        backgroundMusic.Play();
+
+        invincibilityCounter = invincibilityLength;
+        playerRenderer.enabled = false;
+        flashCounter = flashLength;
+    }
+
+
+    public void HealPlayer(int healAmount)
+    {
+        currentHealth += healAmount;
+
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth; 
+        }
+    }
+
+    public void SetSpawnPoint(Vector3 newPosition)
+    {
+        respawnPoint = newPosition;
+    }
+
+}
